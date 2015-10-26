d6bd0a486bc8b81762a47ebcaf28f485d67176d1
jmoller555 <jacquelyn.moller@gmail.com>
2015-10-26 18:02:50 +0000

    Add contact to admin panel

--- core/models.py
+++ core/models.py
@@ -5,3 +5,6 @@ class Contact(models.Model):
   your_email = models.CharField(max_length=300)
   message_for_me = models.TextField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
+
+  def __unicode__(self):
+    return self.your_email
